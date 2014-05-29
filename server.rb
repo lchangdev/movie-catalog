b13d565c8ab008517546cf8dcb5ffc8968fb0809
Lawrence Chang <lc.uclakers@gmail.com>
2014-05-29 11:07:26 -0400

    included pagination, sort, currently cannot do both at the same time, movies and actors data now upload faster due to switching to params

--- server.rb
+++ server.rb
@@ -13,6 +13,10 @@ def db_connection
   end
 end
 
+def next_page(page_num)
+  (page_num - 1) * 20
+end
+
 def actor_names_data
   query = 'SELECT actors.name, actors.id FROM actors
            ORDER BY actors.name'
@@ -22,42 +26,70 @@ def actor_names_data
 end
 
 def actor_profile_data
+  id = params[:id]
 
-  query = 'SELECT actors.id, actors.name, movies.title, cast_members.character, movies.id AS movies_id
-           FROM cast_members
-           JOIN movies ON cast_members.movie_id = movies.id
-           JOIN actors ON cast_members.actor_id = actors.id
-           ORDER BY movies.title'
+  query = 'SELECT actors.id, actors.name, movies.title,
+            cast_members.character, movies.id AS movies_id
+          FROM cast_members
+            JOIN movies ON cast_members.movie_id = movies.id
+            JOIN actors ON cast_members.actor_id = actors.id
+          WHERE actors.id = $1
+          ORDER BY movies.title'
 
   db_connection do |conn|
-    actors = conn.exec(query)
+    actors = conn.exec_params(query, [id])
   end
 end
 
 def movies_data
-  query = 'SELECT movies.title, movies.year, movies.id,
-           movies.rating, genres.name AS genre, studios.name AS studio_name
-           FROM movies
-           JOIN genres ON movies.genre_id = genres.id
-           LEFT OUTER JOIN studios ON movies.studio_id = studios.id
-           ORDER BY movies.title'
+  @page_number = params["page"] || 1
+  @next_page_num = @page_number.to_i + 1
+
+  offset = next_page(@page_number.to_i)
+
+  # if params == {} || params == {"order" => "title"}
+  #   order_by = "movies.title"
+  # elsif params == {"order" => "year"}
+  #   order_by = "movies.year"
+  # elsif params == {"order" => "rating"}
+  #   order_by = "movies.rating"
+  # end
+
+  query = "SELECT movies.title, movies.year, movies.id,
+            movies.rating, genres.name AS genre, studios.name AS studio_name
+          FROM movies
+            JOIN genres ON movies.genre_id = genres.id
+            LEFT OUTER JOIN studios ON movies.studio_id = studios.id
+          ORDER BY movies.title
+          LIMIT 20
+          OFFSET #{offset}"
+
   db_connection do |conn|
     movies = conn.exec(query)
   end
 end
 
 def movie_info
+  id = params[:id]
+
   query = 'SELECT movies.title AS title, genres.name AS genre,
-           studios.name AS studio, actors.name AS name,
-           cast_members.character, movies.id, actors.id AS actors_id
-           FROM movies
-           JOIN genres ON movies.genre_id = genres.id
-           JOIN cast_members ON cast_members.movie_id = movies.id
-           JOIN actors ON cast_members.actor_id = actors.id
-           LEFT OUTER JOIN studios ON movies.studio_id = studios.id'
+            studios.name AS studio, actors.name AS name,
+            cast_members.character, movies.id, actors.id AS actors_id
+          FROM movies
+            JOIN genres ON movies.genre_id = genres.id
+            JOIN cast_members ON cast_members.movie_id = movies.id
+            JOIN actors ON cast_members.actor_id = actors.id
+            LEFT OUTER JOIN studios ON movies.studio_id = studios.id
+          WHERE movies.id = $1'
+
   db_connection do |conn|
-    movies = conn.exec(query)
+    movies = conn.exec_params(query, [id])
   end
+end
+
+get '/' do
+
+  erb :homepage
 end
 
 get '/actors' do
